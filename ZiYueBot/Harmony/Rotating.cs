24491888ee6c85e25b4d56442ab4238d7bf978c6
子悦解说 <ziyuecommentary@gmail.com>
2025-02-26 21:25:41 +0800

    忘了

--- ZiYueBot/Harmony/Rotating.cs
+++ ZiYueBot/Harmony/Rotating.cs
@@ -43,7 +43,7 @@ public class Rotating : IHarmonyCommand
         if (!RateLimit.TryPassRateLimit(this, eventType, userId)) return "频率已达限制（每 3 秒 1 条）";
 
         Logger.Info($"调用者：{userName} ({userId})，参数：{MessageUtils.FlattenArguments(args)}");
-        round.ChamberIndex += 1;
+        Interlocked.Increment(ref round.ChamberIndex);
         if (round.ChamberIndex > RevolverRound.Chambers) StartRevolver.Revolvers.Remove(group, out _);
         return $"已转轮，轮盘中还剩 {round.RestChambers()} 个膛室未击发。{(round.RestChambers() == 0 ? "本局俄罗斯轮盘结束。" : "")}";
     }
