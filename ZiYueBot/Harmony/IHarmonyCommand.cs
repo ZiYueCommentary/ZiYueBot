9e032aababd93c2d00964913459a29388eb0c3af
子悦解说 <ziyuecommentary@gmail.com>
2025-03-01 00:11:52 +0800

    null

--- ZiYueBot/Harmony/IHarmonyCommand.cs
+++ ZiYueBot/Harmony/IHarmonyCommand.cs
@@ -10,7 +10,7 @@ public interface IHarmonyCommand : ICommand
     /// <summary>
     /// 调用鸿蒙命令。
     /// </summary>
-    /// <param name="type">消息来源</param>
+    /// <param name="eventType">消息来源</param>
     /// <param name="userName">调用者的昵称</param>
     /// <param name="userId">调用者的ID</param>
     /// <param name="args">命令的参数</param>
