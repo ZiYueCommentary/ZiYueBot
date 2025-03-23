62c66cfb2ec984f8785e865eebc3768be64b5721
子悦解说 <ziyuecommentary@gmail.com>
2025-03-23 20:34:23 +0800

    天问

--- ZiYueBot/Harmony/Ask.cs
+++ ZiYueBot/Harmony/Ask.cs
@@ -9,6 +9,7 @@ public class Ask : IHarmonyCommand
 {
     private static readonly ILog Logger = LogManager.GetLogger("评价");
     private static readonly List<string> Reviews = [];
+    private static readonly List<(string, List<string>)> AprilReviews = [];
 
     static Ask()
     {
@@ -26,6 +27,74 @@ public class Ask : IHarmonyCommand
         catch (Exception ex)
         {
             Logger.Error("张维为语录库加载失败！", ex);
+        }
+
+        try
+        {
+            using FileStream stream = new FileStream("resources/ziyue.txt", FileMode.OpenOrCreate);
+            using StreamReader reader = new StreamReader(stream);
+            AprilReviews.Add(("子悦", []));
+            while (reader.ReadLine() is { } line)
+            {
+                AprilReviews.Last().Item2.Add(line);
+            }
+
+            Logger.Info("愚人节语录库（子悦）加载完毕");
+        }
+        catch (Exception ex)
+        {
+            Logger.Error("愚人节语录库（子悦）加载失败！", ex);
+        }
+        
+        try
+        {
+            using FileStream stream = new FileStream("resources/easytt.txt", FileMode.OpenOrCreate);
+            using StreamReader reader = new StreamReader(stream);
+            AprilReviews.Add(("义贼哥", []));
+            while (reader.ReadLine() is { } line)
+            {
+                AprilReviews.Last().Item2.Add(line);
+            }
+
+            Logger.Info("愚人节语录库（义贼哥）加载完毕");
+        }
+        catch (Exception ex)
+        {
+            Logger.Error("愚人节语录库（义贼哥）加载失败！", ex);
+        }
+        
+        try
+        {
+            using FileStream stream = new FileStream("resources/asriel.txt", FileMode.OpenOrCreate);
+            using StreamReader reader = new StreamReader(stream);
+            AprilReviews.Add(("山羊", []));
+            while (reader.ReadLine() is { } line)
+            {
+                AprilReviews.Last().Item2.Add(line);
+            }
+
+            Logger.Info("愚人节语录库（山羊）加载完毕");
+        }
+        catch (Exception ex)
+        {
+            Logger.Error("愚人节语录库（山羊）加载失败！", ex);
+        }
+        
+        try
+        {
+            using FileStream stream = new FileStream("resources/capybara.txt", FileMode.OpenOrCreate);
+            using StreamReader reader = new StreamReader(stream);
+            AprilReviews.Add(("水豚哥", []));
+            while (reader.ReadLine() is { } line)
+            {
+                AprilReviews.Last().Item2.Add(line);
+            }
+
+            Logger.Info("愚人节语录库（水豚哥）加载完毕");
+        }
+        catch (Exception ex)
+        {
+            Logger.Error("愚人节语录库（水豚哥）加载失败！", ex);
         }
     }
 
@@ -56,6 +125,18 @@ public class Ask : IHarmonyCommand
     public string Invoke(EventType eventType, string userName, ulong userId, string[] args)
     {
         Logger.Info($"调用者：{userName} ({userId})，参数：{MessageUtils.FlattenArguments(args)}");
+        if (DateTime.Today.Month == 4 && DateTime.Today.Day == 1)
+        {
+            (string, List<string>) aprilReview = AprilReviews[Random.Shared.Next(0, AprilReviews.Count)];
+            if (args.Length >= 2 && args[1] != "")
+            {
+                return
+                    $"{aprilReview.Item1}对 {args[1]} 的评价是：{aprilReview.Item2[Random.Shared.Next(0, aprilReview.Item2.Count - 1)]}";
+            }
+
+            return $"{aprilReview.Item1}的评价是：{aprilReview.Item2[Random.Shared.Next(0, aprilReview.Item2.Count - 1)]}";
+        }
+
         if (args.Length >= 2 && args[1] != "")
         {
             return $"张教授对 {args[1]} 的评价是：{Reviews[Random.Shared.Next(0, Reviews.Count - 1)]}";
