65a50a8cbd1b87a7c3ab106fab3b305bf0641499
ZiYueCommentary <ziyuecommentary@gmail.com>
2025-07-22 14:55:00 +0800

    全新网页版统计数据！

--- ZiYueBot/General/Stat.cs
+++ ZiYueBot/General/Stat.cs
@@ -19,6 +19,7 @@ public class Stat : GeneralCommand
                                           统计你的账号在子悦机器上的数据。
                                           内容包括：所在平台、账号信息、赞助信息、云瓶统计和黑名单信息。
                                           频率限制：每次调用间隔 5 分钟；赞助者 1 分钟。
+                                          完整版统计数据：https://www.ziyuebot.cn/stat.html
                                           在线文档：https://docs.ziyuebot.cn/general/stat
                                           """;
 
@@ -65,25 +66,6 @@ public class Stat : GeneralCommand
             }
         }
 
-        // 海峡云瓶
-        string? straitbottlesStat = null;
-        using (MySqlCommand query = new MySqlCommand($"""
-                                                      SELECT (SELECT COUNT(*) FROM straitbottles WHERE userid = {userId})                    AS bottle_counts,
-                                                             (SELECT COUNT(*) FROM straitbottles WHERE userid = {userId} AND picked = false) AS unpicked_bottles,
-                                                             (SELECT MAX(id) FROM straitbottles)                                             AS last_bottle_id
-                                                      """,
-                   ZiYueBot.Instance.ConnectDatabase()))
-        {
-            using MySqlDataReader reader = query.ExecuteReader();
-            if (reader.Read())
-            {
-                int bottleCounts = reader.GetInt32("bottle_counts");
-                double percent = (double)bottleCounts / reader.GetInt32("last_bottle_id") * 100;
-                straitbottlesStat =
-                    $"您共扔出了 {bottleCounts} 支海峡云瓶，占全部海峡云瓶的 {percent:F4}%，其中有 {reader.GetInt32("unpicked_bottles")} 支仍在海峡漂流。";
-            }
-        }
-
         // 赞助
         string? sponsorStatus = null;
         using (MySqlCommand query = new MySqlCommand(
@@ -106,39 +88,6 @@ public class Stat : GeneralCommand
             }
         }
 
-        // 调用次数
-        string? invokeRecords = null;
-        using (MySqlCommand query = new MySqlCommand(
-                   $"SELECT command, invoke_count FROM invoke_records_general WHERE userid = {userId} AND command IN ('捞云瓶', '捞海峡云瓶', 'ask', 'chat', 'draw')",
-                   ZiYueBot.Instance.ConnectDatabase()))
-        {
-            Dictionary<string, int> commandsInvokeRecords = new Dictionary<string, int>();
-            using MySqlDataReader reader = query.ExecuteReader();
-            while (reader.Read())
-            {
-                commandsInvokeRecords[reader.GetString("command")] = reader.GetInt32("invoke_count");
-            }
-
-            invokeRecords =
-                $"您捞过 {commandsInvokeRecords.GetValueOrDefault("捞云瓶", 0)} 次云瓶和 {commandsInvokeRecords.GetValueOrDefault("捞海峡云瓶", 0)} 次海峡云瓶，{commandsInvokeRecords.GetValueOrDefault("ask", 0)} 次寻求过张教授的智慧（/ask），与子悦机器对话过 {commandsInvokeRecords.GetValueOrDefault("chat", 0)} 次（/chat），让子悦机器画过 {commandsInvokeRecords.GetValueOrDefault("draw", 0)} 幅画（/draw）";
-        }
-
-        // 使用时间
-        string? useTime = null;
-        using (MySqlCommand query =
-               new MySqlCommand(
-                   $"SELECT * FROM invoke_records_general WHERE userid = {userId} ORDER BY first_invoke LIMIT 1",
-                   ZiYueBot.Instance.ConnectDatabase()))
-        {
-            using MySqlDataReader reader = query.ExecuteReader();
-            if (reader.Read())
-            {
-                DateTime firstInvoke = reader.GetDateTime("first_invoke");
-                useTime =
-                    $"从“子悦机器”开源项目立项算起，您在 {firstInvoke:yyyy年MM月dd日} 第一次调用子悦机器，距今 {(DateTime.Today - firstInvoke.Date).Days} 天。";
-            }
-        }
-
         // 俄罗斯轮盘
         string? revolverStat = null;
         using (MySqlCommand query = new MySqlCommand($"SELECT * FROM revolver WHERE userid = {userId}",
@@ -156,7 +105,7 @@ public class Stat : GeneralCommand
                     revolverStat =
                         $"您在 {reader.GetDateTime("first_invoke"):yyyy年MM月dd日} 第一次调用俄罗斯轮盘命令，开始过 {reader.GetInt32("start_count")} 局轮盘，转轮 {reader.GetInt32("rotating_count")} 次，重置 {reader.GetInt32("restart_count")} 次。";
                     revolverStat +=
-                        $"您向别人开过 {shootingOtherCount} 枪，其中打死过 {shootingOtherDeath} 次。您向自己开过 {shootingSelfCount} 次枪，其中打死过 {shootingSelfDeath} 次。总射击准度 {((shootingOtherDeath + shootingSelfDeath) / (shootingOtherCount + shootingSelfCount) * 100):F4}%";
+                        $"您向别人开过 {shootingOtherCount} 枪，其中打死过 {shootingOtherDeath} 次。您向自己开过 {shootingSelfCount} 次枪，其中打死过 {shootingSelfDeath} 次。总射击准度 {((shootingOtherDeath + shootingSelfDeath) / (shootingOtherCount + shootingSelfCount) * 100):F4}%。";
                 }
             }
         }
@@ -187,11 +136,10 @@ public class Stat : GeneralCommand
                 {sponsorStatus ?? "您不是子悦机器的赞助者。"}
                 {driftbottlesStat ?? "云瓶统计失败，请联系子悦。"}
                 {driftbottlesIncrementalStat ?? "云瓶增长统计失败，请联系子悦。"}
-                {straitbottlesStat ?? "海峡云瓶统计失败，请联系子悦。"}
-                {invokeRecords ?? "这怎么可能？命令调用统计失败。"}
-                {useTime ?? "找不到调用记录"}
                 {revolverStat ?? "您没有调用过俄罗斯轮盘命令。"}
                 {blacklists ?? "您没有被列入黑名单的命令。"}
+                
+                完整版统计数据另见：https://www.ziyuebot.cn/stat.html?id={userId}
                 """;
     }
 
