1d5849a2fc963787db350c014f86fc4437a56e7f
ZiYueCommentary <ziyuecommentary@gmail.com>
2026-07-09 01:36:11 +0800

    剥离数据库初始化语句

--- ZiYueBot/ZiYueBot.cs
+++ ZiYueBot/ZiYueBot.cs
@@ -40,14 +40,15 @@ public class ZiYueBot
 
         Discord = new DiscordSocketClient(new DiscordSocketConfig
         {
-            WebSocketProvider = DefaultWebSocketProvider.Create(new WebProxy(Config.DiscordProxy))
+            RestClientProvider = DefaultRestClientProvider.Create(true),
+            WebSocketProvider =
+                DefaultWebSocketProvider.Create(new WebProxy(Environment.GetEnvironmentVariable("HTTPS_PROXY")))
         });
         Discord.LoginAsync(TokenType.Bot, Config.DiscordToken).Wait();
         Discord.StartAsync().Wait();
         // Logger.Info($"Discord 登录成功：{Discord.CurrentUser.GlobalName} ({Discord.CurrentUser.Id})");
 
         InitializeDatabase();
-        Logger.Info("MySQL 初始化完毕");
     }
 
     internal void ConnectQqWebSocket()
@@ -68,253 +69,18 @@ public class ZiYueBot
 
     private void InitializeDatabase()
     {
-        using MySqlConnection database = ConnectDatabase();
-
         try
         {
-            MySqlCommand command = new MySqlCommand("""
-                                                    CREATE TABLE driftbottles
-                                                    (
-                                                        id       int auto_increment primary key,
-                                                        userid   bigint                    null,
-                                                        username tinytext                  null,
-                                                        created  datetime                  null,
-                                                        content  text                      null,
-                                                        pickable boolean           default true,
-                                                        views    int                  default 0
-                                                    ) CHARSET = utf8mb4;
-                                                    """, database);
+            using FileStream stream = new FileStream("resources/initialize.sql", FileMode.OpenOrCreate);
+            using StreamReader reader = new StreamReader(stream);
+            MySqlCommand command = new MySqlCommand(reader.ReadToEnd(), ConnectDatabase());
             command.ExecuteNonQuery();
+            Logger.Info("数据库初始化成功");
         }
-        catch (MySqlException)
+        catch (Exception e)
         {
-        }
-
-        try
-        {
-            MySqlCommand command = new MySqlCommand("""
-                                                    CREATE TABLE straitbottles
-                                                    (
-                                                        id            int auto_increment primary key,
-                                                        userid        bigint                    null,
-                                                        username      tinytext                  null,
-                                                        created       datetime                  null,
-                                                        content       text                      null,
-                                                        fromDiscord   boolean                   null,
-                                                        picked        boolean          default false,
-                                                        picked_time   datetime                  null
-                                                    ) CHARSET = utf8mb4;
-                                                    """, database);
-            command.ExecuteNonQuery();
-        }
-        catch (MySqlException)
-        {
-        }
-
-        try
-        {
-            MySqlCommand command = new MySqlCommand("""
-                                                    CREATE TABLE win
-                                                    (
-                                                        userid                bigint      default 0,
-                                                        username              tinytext         null,
-                                                        channel               bigint      default 0,
-                                                        date                  date             null,
-                                                        score                 tinyint          null,
-                                                        prospered             boolean default false,
-                                                        miniWinDays           tinyint     default 0,
-                                                        invoke_days           int         default 0,
-                                                        flexible_win_days     int         default 0,
-                                                        mini_win_days         int         default 0,
-                                                        middle_win_days       int         default 0,
-                                                        big_win_days          int         default 0,
-                                                        very_big_win_days     int         default 0,
-                                                        ultra_win_days        int         default 0,
-                                                        lose_days             int         default 0,
-                                                        couple_win_days       int         default 0,
-                                                        wind_window_days      int         default 0,
-                                                        alleviated_days       int         default 0,
-                                                        prosperity_days       int         default 0,
-                                                        prosperity_other_days int         default 0,
-                                                        PRIMARY KEY (userid, channel)
-                                                    ) CHARSET = utf8mb4;
-                                                    """, database);
-            command.ExecuteNonQuery();
-        }
-        catch (MySqlException)
-        {
-        }
-
-        try
-        {
-            MySqlCommand command = new MySqlCommand("""
-                                                    CREATE TABLE aprilbottles
-                                                    (
-                                                        id       int auto_increment primary key,
-                                                        username tinytext                  null,
-                                                        created  date                      null,
-                                                        content  text                      null
-                                                    ) CHARSET = utf8mb4;
-                                                    """, database);
-            command.ExecuteNonQuery();
-        }
-        catch (MySqlException)
-        {
-        }
-
-        try
-        {
-            MySqlCommand command = new MySqlCommand("""
-                                                    CREATE TABLE blacklists
-                                                    (
-                                                        userid  bigint          default 0,
-                                                        command varchar(50) default 'all',
-                                                        time    datetime             null,
-                                                        reason  text                 null,
-                                                        PRIMARY KEY (userid, command)
-                                                    ) CHARSET = utf8mb4;
-                                                    """, database);
-            command.ExecuteNonQuery();
-        }
-        catch (MySqlException)
-        {
-        }
-
-        try
-        {
-            MySqlCommand command = new MySqlCommand("""
-                                                    CREATE TABLE sponsors
-                                                    (
-                                                        userid  bigint default 0 primary key,
-                                                        expiry  date                    null
-                                                    ) CHARSET = utf8mb4;
-                                                    """, database);
-            command.ExecuteNonQuery();
-        }
-        catch (MySqlException)
-        {
-        }
-
-        try
-        {
-            MySqlCommand command = new MySqlCommand("""
-                                                    CREATE TABLE draw
-                                                    (
-                                                        userid        bigint default 0 primary key,
-                                                        current_month date                    null,
-                                                        limitation    int                     null,
-                                                        consumed      int                     null
-                                                    ) CHARSET = utf8mb4;
-                                                    """, database);
-            command.ExecuteNonQuery();
-        }
-        catch (MySqlException)
-        {
-        }
-
-        try
-        {
-            MySqlCommand command = new MySqlCommand("""
-                                                    CREATE TABLE invoke_records_general
-                                                    (
-                                                        userid       bigint      not null,
-                                                        command      varchar(50) not null,
-                                                        first_invoke datetime        null,
-                                                        last_invoke  datetime        null,
-                                                        invoke_count int             null,
-                                                        PRIMARY KEY (userid, command)
-                                                    ) CHARSET = utf8mb4;
-
-                                                    CREATE INDEX invoke_records_general_index_userid
-                                                    ON invoke_records_general (userid);
-                                                    """, database);
-            command.ExecuteNonQuery();
-        }
-        catch (MySqlException)
-        {
-        }
-
-        try
-        {
-            MySqlCommand command = new MySqlCommand("""
-                                                    CREATE TABLE revolver
-                                                    (
-                                                        userid               bigint   not null primary key,
-                                                        first_invoke         datetime                 null,
-                                                        last_invoke          datetime                 null,
-                                                        start_count          int                 default 0,
-                                                        shooting_self_count  int                 default 0,
-                                                        shooting_other_count int                 default 0,
-                                                        shooting_self_death  int                 default 0,
-                                                        shooting_other_death int                 default 0,
-                                                        rotating_count       int                 default 0,
-                                                        restart_count        int                 default 0,
-                                                        being_shot           int                 default 0
-                                                    ) CHARSET = utf8mb4;
-                                                    """, database);
-            command.ExecuteNonQuery();
-        }
-        catch (MySqlException)
-        {
-        }
-
-        try
-        {
-            MySqlCommand command = new MySqlCommand("""
-                                                    CREATE TABLE driftbottles_queue
-                                                    (
-                                                        queue_id int auto_increment primary key,
-                                                        userid   bigint                    null,
-                                                        username tinytext                  null,
-                                                        created  datetime                  null,
-                                                        content  text                      null,
-                                                        reviewed tinyint(1)           default 0
-                                                    ) CHARSET = utf8mb4;
-                                                    """, database);
-            command.ExecuteNonQuery();
-        }
-        catch (MySqlException)
-        {
-        }
-
-        try
-        {
-            MySqlCommand command = new MySqlCommand("""
-                                                    CREATE TABLE stargazers
-                                                    (
-                                                        userid    bigint               default 0,
-                                                        star_at   datetime                  null,
-                                                        bottle_id int                  default 0,
-                                                        removed   tinyint(1)           default 0,
-                                                        PRIMARY KEY (userid, bottle_id)
-                                                    ) CHARSET = utf8mb4;
-
-                                                    CREATE INDEX stargazers_bottle_id_index
-                                                        ON stargazers (bottle_id, removed);
-
-                                                    CREATE INDEX stargazers_userid_index
-                                                        ON stargazers (userid, removed);
-                                                    """, database);
-            command.ExecuteNonQuery();
-        }
-        catch (MySqlException)
-        {
-        }
-
-        try
-        {
-            MySqlCommand command = new MySqlCommand("""
-                                                    CREATE TABLE privileges
-                                                    (
-                                                        userid        bigint not null primary key,
-                                                        flags         bigint                 null,
-                                                        active_counts int                    null
-                                                    ) CHARSET = utf8mb4;
-                                                    """, database);
-            command.ExecuteNonQuery();
-        }
-        catch (MySqlException)
-        {
+            // 垃圾 MySQL 不能对索引使用 IF NOT EXISTS
+            // Logger.Error("数据库初始化出错", e);
         }
     }
 
---
+++ ZiYueBot/resources/initialize.sql
@@ -0,0 +1,148 @@
+# 子悦机器数据库初始化
+# 以下数据库表为 MySQL 5.7 设计，任何比 MySQL 正经的数据库应该都可以用
+
+# 漂流云瓶
+CREATE TABLE IF NOT EXISTS driftbottles
+(
+    id       int auto_increment primary key,
+    userid   bigint   null,
+    username tinytext null,
+    created  datetime null,
+    content  text     null,
+    pickable boolean default true,
+    views    int     default 0
+) CHARSET = utf8mb4;
+
+# 海峡云瓶
+CREATE TABLE IF NOT EXISTS straitbottles
+(
+    id          int auto_increment primary key,
+    userid      bigint   null,
+    username    tinytext null,
+    created     datetime null,
+    content     text     null,
+    fromDiscord boolean  null,
+    picked      boolean default false,
+    picked_time datetime null
+) CHARSET = utf8mb4;
+
+# 赢
+CREATE TABLE IF NOT EXISTS win
+(
+    userid                bigint  default 0,
+    username              tinytext null,
+    channel               bigint  default 0,
+    date                  date     null,
+    score                 tinyint  null,
+    prospered             boolean default false,
+    miniWinDays           tinyint default 0,
+    invoke_days           int     default 0,
+    flexible_win_days     int     default 0,
+    mini_win_days         int     default 0,
+    middle_win_days       int     default 0,
+    big_win_days          int     default 0,
+    very_big_win_days     int     default 0,
+    ultra_win_days        int     default 0,
+    lose_days             int     default 0,
+    couple_win_days       int     default 0,
+    wind_window_days      int     default 0,
+    alleviated_days       int     default 0,
+    prosperity_days       int     default 0,
+    prosperity_other_days int     default 0,
+    PRIMARY KEY (userid, channel)
+) CHARSET = utf8mb4;
+
+# 愚人云瓶
+CREATE TABLE IF NOT EXISTS aprilbottles
+(
+    id       int auto_increment primary key,
+    username tinytext null,
+    created  date     null,
+    content  text     null
+) CHARSET = utf8mb4;
+
+# 黑名单
+CREATE TABLE IF NOT EXISTS blacklists
+(
+    userid  bigint      default 0,
+    command varchar(50) default 'all',
+    time    datetime null,
+    reason  text     null,
+    PRIMARY KEY (userid, command)
+) CHARSET = utf8mb4;
+
+# 赞助者
+CREATE TABLE IF NOT EXISTS sponsors
+(
+    userid bigint default 0 primary key,
+    expiry date null
+) CHARSET = utf8mb4;
+
+# 绘画额度
+CREATE TABLE IF NOT EXISTS draw
+(
+    userid        bigint default 0 primary key,
+    current_month date null,
+    limitation    int  null,
+    consumed      int  null
+) CHARSET = utf8mb4;
+
+# 调用统计
+CREATE TABLE IF NOT EXISTS invoke_records_general
+(
+    userid       bigint      not null,
+    command      varchar(50) not null,
+    first_invoke datetime    null,
+    last_invoke  datetime    null,
+    invoke_count int         null,
+    PRIMARY KEY (userid, command)
+) CHARSET = utf8mb4;
+CREATE INDEX invoke_records_general_index_userid ON invoke_records_general (userid);
+
+# 俄罗斯轮盘
+CREATE TABLE IF NOT EXISTS revolver
+(
+    userid               bigint   not null primary key,
+    first_invoke         datetime null,
+    last_invoke          datetime null,
+    start_count          int default 0,
+    shooting_self_count  int default 0,
+    shooting_other_count int default 0,
+    shooting_self_death  int default 0,
+    shooting_other_death int default 0,
+    rotating_count       int default 0,
+    restart_count        int default 0,
+    being_shot           int default 0
+) CHARSET = utf8mb4;
+
+# 云瓶审核队列
+# 以下为最基本的表结构，子悦机器实际上还有更多私有数据
+CREATE TABLE IF NOT EXISTS driftbottles_queue
+(
+    queue_id int auto_increment primary key,
+    userid   bigint   null,
+    username tinytext null,
+    created  datetime null,
+    content  text     null,
+    reviewed tinyint(1) default 0
+) CHARSET = utf8mb4;
+
+# 云瓶星标
+CREATE TABLE IF NOT EXISTS stargazers
+(
+    userid    bigint     default 0,
+    star_at   datetime null,
+    bottle_id int        default 0,
+    removed   tinyint(1) default 0,
+    PRIMARY KEY (userid, bottle_id)
+) CHARSET = utf8mb4;
+CREATE INDEX stargazers_bottle_id_index ON stargazers (bottle_id, removed);
+CREATE INDEX stargazers_userid_index ON stargazers (userid, removed);
+
+# 特权
+CREATE TABLE IF NOT EXISTS privileges
+(
+    userid        bigint not null primary key,
+    flags         bigint null,
+    active_counts int    null
+) CHARSET = utf8mb4;
