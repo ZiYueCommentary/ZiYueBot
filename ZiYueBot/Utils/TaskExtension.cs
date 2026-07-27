bf00363c4536bd88c26ed3ac1f136512a5266b3d
EasyT_T <116633144+EasyT-T@users.noreply.github.com>
2026-07-27 15:00:59 +0800

    并行DiscordHandler (#18)
    * 并行DiscordHandler
    
    * 无异常处理器时忽略异常防止向上传播

---
+++ ZiYueBot/Utils/TaskExtension.cs
@@ -0,0 +1,41 @@
+﻿namespace ZiYueBot.Utils;
+
+public static class TaskExtension
+{
+    public static void Forget(this Task task, Action<Exception>? exceptionHandler = null)
+    {
+        HandleFireAndForget(task, exceptionHandler);
+    }
+
+    public static void Forget(this Task task, Func<Exception, Task>? exceptionHandler = null)
+    {
+        HandleFireAndForget(task, exceptionHandler);
+    }
+
+    private static async void HandleFireAndForget(Task task, Action<Exception>? exceptionHandler = null)
+    {
+        try
+        {
+            await task.ConfigureAwait(false);
+        }
+        catch (Exception e)
+        {
+            exceptionHandler?.Invoke(e);
+        }
+    }
+
+    private static async void HandleFireAndForget(Task task, Func<Exception, Task>? exceptionHandler = null)
+    {
+        try
+        {
+            await task.ConfigureAwait(false);
+        }
+        catch (Exception e)
+        {
+            if (exceptionHandler is not null)
+            {
+                await exceptionHandler.Invoke(e);
+            }
+        }
+    }
+}
